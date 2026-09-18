d54fd79364e8ce00ee292d8ba8040925063e2e26
JaradSpencer <audiospencer@gmail.com>
2026-09-18 14:39:24 -0500

    Spencer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Left from './components/LeftNav'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -17,23 +21,40 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo or not.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <body>
+      <>
+        <Header />
+      </>
+
+      <main class="main-content">
+        <Left />
+
+        <h1>My List</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}>{todo.content}</li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+            Review next step of this tutorial.
+          </a>
+        </div>
+
+
+      </main>
+      <Footer />
+    </body>
+
   );
 }
 

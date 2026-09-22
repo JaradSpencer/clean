4db08efdb0515df3a14849bca682b12e17889bb2
JaradSpencer <audiospencer@gmail.com>
2026-09-22 12:25:36 -0500

    No functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+
 
 
 
@@ -8,24 +6,12 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Left from './components/LeftNav'
 
-const client = generateClient<Schema>();
+
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
   return (
     <body>
       <>
@@ -35,22 +21,8 @@ function App() {
       <main className="main-content">
         <Left />
 
-        <h1>My List</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li
-              onClick={() => deleteTodo(todo.id)}
-              key={todo.id}>{todo.content}</li>
-          ))}
-        </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
-        </div>
+       
+
 
 
       </main>
--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -3,11 +3,11 @@ export default function LeftNav() {
         <nav className="sidebar">
 
             <ul className="sidebar-menu">
-                <li><a href="#" className="active">Dashboard</a></li>
-                <li><a href="#">Analytics</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Messages</a></li>
-                <li><a href="#">Settings</a></li>
+                
+                <li><a href="#">AMX</a></li>
+                <li><a href="#">Extron</a></li>
+                <li><a href="#">Crestron</a></li>
+                <li><a href="#">QSys</a></li>
             </ul>
         </nav>
 
